839470bb3056a5a98767b2835a6bf3a8e6555fd3
aryanpnd <captainaryan9870@gmail.com>
2025-02-24 23:24:09 +0530

    Add drives feature and advance message search: implement fetching and display of friend's drives; update UI components and constants

--- backend/api-v1/src/services/saveToDB/studentDetails.ts
+++ backend/api-v1/src/services/saveToDB/studentDetails.ts
@@ -3,26 +3,35 @@ import { StudentDetails } from "../../types/DB_ServicesTypes";
 
 export const saveStudentDetails = async (studentDetails: StudentDetails) => {
     try {
+        const existingStudent = await Student.findOne({ reg_no: studentDetails.reg_no });
+
+        if (!existingStudent) {
+            studentDetails.allowedFieldsToShow = ["timetable"];
+        } else {
+            studentDetails.allowedFieldsToShow = existingStudent.allowedFieldsToShow;
+        }
+
         const student = await Student.findOneAndUpdate(
             { reg_no: studentDetails.reg_no },
             studentDetails,
             {
-                new: false,
-                upsert: true, // create if doesn't exist
-                runValidators: false // run model validators
+                new: true, 
+                upsert: true,
+                runValidators: false,
             }
         );
+
         return {
             success: true,
             message: "Student details saved successfully",
             data: student
-        }
+        };
     } catch (error: any) {
         console.error(error.message);
         return {
             success: false,
             message: error.message,
             data: {}
-        }
+        };
     }
-}
+};
